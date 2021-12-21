5d1f01383e2dbb5a345f20a9a31e37750c63d6b5
Petr Tripolsky <tripolskypetr@gmail.com>
2021-12-21 14:59:55 +0300

    better-install-script

--- scripts/install-opencv-contrib.sh
+++ scripts/install-opencv-contrib.sh
@@ -1,3 +1,5 @@
+apt update
+apt install -y cmake gcc bison pkg-config autoconf python3 python3-distutils gperf libx11-dev libxft-dev libxext-dev libtool libglfw3 libgles2-mesa-dev libxrandr-dev libxi-dev libxcursor-dev libxinerama-dev libxxf86vm-dev
 rm -rf tmp
 mkdir tmp && cd tmp
 git clone --branch 3.4 --depth 1 https://github.com/opencv/opencv.git opencv-master
@@ -9,6 +11,6 @@ cmake \
     ../opencv-master
 cmake --build .
 make -j8
-sudo make install
-sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf' 
-sudo ldconfig
+make install
+sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf' 
+ldconfig
